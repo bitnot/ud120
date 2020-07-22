91ffac9d7ed1650f00f03c3759ef90b3a7039f65
bitnot <bitnot01@gmail.com>
2020-07-22 11:49:20 +0200

    Add Naive Bayes excercise

--- naive_bayes/emails/nb_author_id.py
+++ naive_bayes/emails/nb_author_id.py
@@ -27,6 +27,37 @@ features_train, features_test, labels_train, labels_test = preprocess(
 
 
 #########################################################
+def timeit(code, label=""):
+    t0 = time()
+    result = code()
+    print(f"time {label}: {round(time()-t0, 3)}s")
+    return result
+
 ### your code goes here ###
 
+
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.metrics import accuracy_score
+
+    # create classifier
+    clf = GaussianNB()
+
+    # fit the classifier on the training features and labels
+    timeit(lambda: clf.fit(features_train, labels_train), "fit")
+
+    # use the trained classifier to predict labels for the test features
+    labels_pred = timeit(lambda: clf.predict(features_test), "predict")
+
+    # calculate and return the accuracy on the test data
+    # this is slightly different than the example,
+    # where we just print the accuracy
+    # you might need to import an sklearn module
+    accuracy = accuracy_score(labels_test, labels_pred)
+    return accuracy
+
+
+print(
+    f"Accuracy is {NBAccuracy(features_train, labels_train, features_test, labels_test)}")
 #########################################################
