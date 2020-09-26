21bd4c2ade13218982d7cfbf672a62d80881af5c
bitnot <bitnot01@gmail.com>
2020-09-26 14:23:32 +0200

    Add basic decision tree classifier

---
+++ decision_trees/classifier/quiz4_8.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+""" lecture and example code for decision tree unit """
+
+import sys
+sys.path.insert(0,'../../lib')
+
+from class_vis import prettyPicture, output_image
+from prep_terrain_data import makeTerrainData
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab as pl
+from classifyDT import classify
+from sklearn.metrics import accuracy_score
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+
+### the classify() function in classifyDT is where the magic
+### happens--fill in this function in the file 'classifyDT.py'!
+clf = classify(features_train, labels_train)
+
+
+# store your predictions in a list named pred
+pred = clf.predict(features_test)
+
+
+acc = accuracy_score(pred, labels_test)
+
+
+def submitAccuracy():
+    return acc
+
+print(acc)
+
+#### grader code, do not modify below this line
+
+prettyPicture(clf, features_test, labels_test)
+output_image('test.png')
